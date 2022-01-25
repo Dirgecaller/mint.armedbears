c3952aaaf8f45909fef85708109042eb5684b817
JoshBauguss <josh@zebotech.com>
2022-01-24 18:19:35 -0700

    commit all the files

---
+++ src/App.js
@@ -0,0 +1,209 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+
+import styled from "styled-components";
+
+export const StyledButton = styled.button;
+
+
+function App() {
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
+  const data = useSelector((state) => state.data);
+  const [feedback, setFeedback] = useState("");
+  const [claimingNft, setClaimingNft] = useState(false);
+  const claimNFTs = (_amount) => {
+    _amount = document.getElementById("inputBox").value;
+    if (_amount <= 0) {
+      return;
+    }
+    setFeedback("Minting your Armed Bear NFT...");
+    setClaimingNft(true);
+    
+    blockchain.smartContract.methods
+      .preSaleMint(_amount)
+      // ********
+      // You can change the line above to
+      // .whiteListMint(blockchain.account, _amount) if you want only whitelisted
+      // users to be able to mint through your website!
+      // And after you're done with whitelisted users buying from your website,
+      // You can switch it back to .mint(blockchain.account, _amount).
+      // ********
+      .send({
+        gasLimit: 485000 * _amount,
+        to: "0x0b2CD470d9d97096a3F591F6B2D6f2907754d15f",
+        from: blockchain.account,
+        value: blockchain.web3.utils.toWei((100 * _amount).toString(), "ether"),
+      })
+      .once("error", (err) => {
+        
+        if(err.code === 4001) {
+          setFeedback("We were told you rejected the transaction.  Sorry???");
+        } else {
+          setFeedback("Sorry, something went wrong. Check your transaction on Etherscan to find out what happened!");
+        }
+        
+        
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        setFeedback(
+          "CONGRATS! Your NFT successfully minted!"
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+      
+      //string: https://mint.armedbears.io/api/6
+
+      
+      //contract address on ropsetn
+      //0x0567b8272df3fB4E3b0cF1f50FfdAA1A1BC766B8
+    /*
+    blockchain.smartContract.methods
+      .mint(blockchain.account, _amount)
+      // ********
+      // You can change the line above to
+      // .whiteListMint(blockchain.account, _amount) if you want only whitelisted
+      // users to be able to mint through your website!
+      // And after you're done with whitelisted users buying from your website,
+      // You can switch it back to .mint(blockchain.account, _amount).
+      // ********
+      .send({
+        gasLimit: 285000 * _amount,
+        to: "0x0567b8272df3fB4E3b0cF1f50FfdAA1A1BC766B8",
+        from: blockchain.account,
+        value: blockchain.web3.utils.toWei((0.0308 * _amount).toString(), "ether"),
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("Sorry, something went wrong. Check your transaction on Etherscan to find out what happened!");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        setFeedback(
+          "CONGRATS! Your NFT successfully minted!"
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+      */
+  };
+
+  const getData = () => {
+    if (blockchain.account !== "" && blockchain.smartContract !== null) {
+      dispatch(fetchData(blockchain.account));
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [blockchain.account]);
+
+  return (
+    <div className="app-wrapper">
+      <div className="inner">
+        
+        <div className="inner-2">
+          <div className="inner-3">
+          
+            <div className="counter">
+              Counter: {blockchain.account == null ? "0" : (data.totalSupply)}/10000
+            </div>
+            
+            <div>
+            {Number(data.totalSupply) === 10000 ? (
+              <>
+                <div className="sale-ended">
+                  The sale has ended.
+                </div>
+                {/* <s.SpacerSmall /> */}
+                <div className="opensea-link">
+                  Dont worry, you're not missing out! You can still get Armed Bears on{" "}
+                  <a
+                    // target={"_blank"}
+                    href={"https://testnets.opensea.io/collection/"}
+                  >
+                    Opensea.io
+                  </a>
+                </div>
+              </>
+            ) : (
+              <>
+
+                <div className="feedback">
+                  {feedback}
+                </div>
+                {/* <s.SpacerMedium /> */}
+                {blockchain.account === "" ||
+                blockchain.smartContract === null ? (
+                  <div className="wallet-button">
+
+                    <button className="wallet-connect-button"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(connect());
+                        getData();
+                      }}
+                    >
+                      Connect to Polygon Wallet
+                    </button>
+                    
+                    {/* <s.TextDescription style={{textAlign: "center", fontSize: 30, marginBottom: 0, paddingBottom: 0}}>
+                      <a href="https://google.com">Boo Crew NFT Smart Contract</a>
+                    </s.TextDescription> */}
+                    {blockchain.errorMsg !== "" ? (
+                      <>
+                        <div className="blockchain-error">
+                          {blockchain.errorMsg}
+                        </div>
+                      </>
+                    ) : null}
+                  </div>
+                ) : (
+                  <div>
+                    <form className="mint-form">
+                    {/* I want  */}
+                    <input
+                    id="inputBox"
+                    placeholder="#"
+                    type="number"
+                    min="1"
+                    max="5"
+                    
+                    />
+                    
+                    </form>
+                    
+                    <button
+                      className="mint-button"
+                      disabled={claimingNft ? 1 : 0}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        claimNFTs(1);
+                        getData();
+                      }}
+                    >
+                      {claimingNft ? "BUSY" : "MINT"}
+                    </button>
+                  </div>
+                )}
+              </>
+            )}
+            </div>
+          
+              
+          </div>
+          
+          
+        </div>
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/blockchain/blockchainActions.js
@@ -0,0 +1,104 @@
+// constants
+import Web3EthContract from "web3-eth-contract";
+import Web3 from "web3";
+import SmartContract from "../../contracts/ContractABI.json";
+// log
+import { fetchData } from "../data/dataActions";
+
+const connectRequest = () => {
+  return {
+    type: "CONNECTION_REQUEST",
+  };
+};
+
+const connectSuccess = (payload) => {
+  return {
+    type: "CONNECTION_SUCCESS",
+    payload: payload,
+  };
+};
+
+const connectFailed = (payload) => {
+  return {
+    type: "CONNECTION_FAILED",
+    payload: payload,
+  };
+};
+
+const updateAccountRequest = (payload) => {
+  return {
+    type: "UPDATE_ACCOUNT",
+    payload: payload,
+  };
+};
+
+export const connect = () => {
+  return async (dispatch) => {
+    dispatch(connectRequest());
+    const { ethereum } = window;
+    const metamaskIsInstalled = ethereum && ethereum.isMetaMask;
+    if (metamaskIsInstalled) {
+      Web3EthContract.setProvider(ethereum);
+      let web3 = new Web3(ethereum);
+      try {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const networkId = await ethereum.request({
+          method: "net_version",
+        });
+        
+        console.log(networkId);
+        
+        // const NetworkData = await SmartContract.networks[networkId];
+        /*
+          on ethereum network networkId will be...
+            - 1 is mainnet
+            - 4 is rinkeby testnet
+            - 3 is ropsten testnet
+        */
+        
+        /*
+          on polygon networkId will be...
+            - 137 is polygon mainnet
+            - 80001 is mumbai testnet
+        */
+        
+        if (networkId === "80001") { // 1 is mainnet. 4 is rinkeby testnet.  3 is for ropsten
+          const SmartContractObj = new Web3EthContract(
+            SmartContract,
+            "0x0b2CD470d9d97096a3F591F6B2D6f2907754d15f"
+          );
+          dispatch(
+            connectSuccess({
+              account: accounts[0],
+              smartContract: SmartContractObj,
+              web3: web3,
+            })
+          );
+          // Add listeners start
+          ethereum.on("accountsChanged", (accounts) => {
+            dispatch(updateAccount(accounts[0]));
+          });
+          ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+          // Add listeners end
+        } else {
+          dispatch(connectFailed("Change network to Polygon."));
+        }
+      } catch (err) {
+        dispatch(connectFailed("Something went wrong."));
+      }
+    } else {
+      dispatch(connectFailed("Please install a Polygon compatible wallet."));
+    }
+  };
+};
+
+export const updateAccount = (account) => {
+  return async (dispatch) => {
+    dispatch(updateAccountRequest({ account: account }));
+    dispatch(fetchData(account));
+  };
+};
